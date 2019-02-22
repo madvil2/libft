1c59aa7a16449b83fe4f418e04061bf4f1a7dd05
madvil2 <madvil2@mail.ru>
2019-02-23 01:45:45 +0300

    update gnl

--- get_next_line.c
+++ get_next_line.c
@@ -6,54 +6,37 @@
 /*   By: pcollio- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/13 22:02:40 by pcollio-          #+#    #+#             */
-/*   Updated: 2019/02/22 16:55:24 by pcollio-         ###   ########.fr       */
+/*   Updated: 2019/02/23 01:45:12 by pcollio-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static int	cut_lines(char **line, char *multy)
-{
-	char *str;
-
-	str = ft_strchr(multy, '\n');
-	if (str)
-	{
-		str[0] = '\0';
-		*line = ft_strdup(multy);
-		ft_strcpy(multy, str + 1);
-		return (1);
-	}
-	else if (ft_strlen(multy) > 0)
-	{
-		*line = ft_strdup(multy);
-		*multy = 0;
-		return (1);
-	}
-	return (0);
-}
-
-int			get_next_line(const int fd, char **line)
+int		get_next_line(const int fd, char **line)
 {
 	static char	*multy[10240];
-	char		buffer[BUFF_SIZE + 1];
-	ssize_t		rd;
-	char		*temp;
+	char		buf[BUFF_SIZE + 1];
+	int			rd;
+	char		*str;
 
-	if (fd < 0 || fd > 10240 || !line)
-		return (-1);
-	if (!multy[fd] && !(multy[fd] = ft_strnew(1)))
-		return (-1);
-	while (!ft_strchr(multy[fd], '\n'))
+	while ((!(!(line)) || (rd != -1)) &&
+			(rd = read(fd, buf, BUFF_SIZE)) > 0)
 	{
-		if ((rd = read(fd, buffer, BUFF_SIZE)) == -1)
-			return (-1);
-		if (rd == 0)
-			break ;
-		buffer[rd] = '\0';
-		temp = ft_strjoin(multy[fd], buffer);
-		free(multy[fd]);
-		multy[fd] = temp;
+		buf[rd] = '\0';
+		str = multy[fd];
+		if (!(multy[fd] = ft_strjoin(multy[fd], buf)))
+			multy[fd] = ft_strdup(buf);
+		ft_strdel(&str);
 	}
-	return (cut_lines(line, multy[fd]));
+	if ((rd == -1 || !ft_strlen(multy[fd])) && (!line || (*line = ft_strnew(0))))
+		return (rd);
+	str = ft_strchr(multy[fd], '\n');
+	if (str && !(*str = '\0'))
+		str = ft_strdup(str + 1);
+	else
+		str = ft_strnew(0);
+	*line = ft_strdup(multy[fd]);
+	free(multy[fd]);
+	multy[fd] = str;
+	return (1);
 }
