d124446c9aee182e064c9a382c371b5dc9ff0cdf
Kostiantyn Kaimov <kokaimov@c1b4c1.42berlin.de>
2024-04-07 14:27:13 +0200

    fix norme

--- libft.h
+++ libft.h
@@ -69,7 +69,8 @@ int				ft_lstsize(t_list *lst);
 t_list			*ft_lstlast(t_list *lst);
 void			ft_lstdelone(t_list *lst, void (*del)(void *));
 void			ft_lstiter(t_list *lst, void (*f)(void *));
-t_list			*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *));
+t_list			*ft_lstmap(t_list *lst, void *(*f)(void *),
+					void (*del)(void *));
 
 // additional
 int				ft_putnbr_base(long nbr, char *base);
